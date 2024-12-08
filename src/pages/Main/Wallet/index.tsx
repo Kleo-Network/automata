5c4c096bfcd9d23dc89a6e64a0c0e37e29431361
princedalsaniya <prince.d@ahduni.edu.in>
2024-12-09 01:22:14 +0530

    feat: added transactions list

--- src/pages/Main/Wallet/index.tsx
+++ src/pages/Main/Wallet/index.tsx
@@ -8,7 +8,7 @@ const WALLET_PAGE_DATA = {
   yourBalance: 'Your balance is',
   points: 1453,
   xpPoints: 'XP Points',
-  transactions: [
+  transactionCards: [
     {
       title: 'Total Spent',
       value: 1146
@@ -17,6 +17,40 @@ const WALLET_PAGE_DATA = {
       title: 'Total Recieved',
       value: 2599
     },
+  ],
+  title: "Previous Transactions",
+  desc: 'Select your task and sit back as your personal ai assistant take over.',
+  transactions: [
+    {
+      type: 'OUTGO',
+      desc: 'Spent on Amazon buy Ps5',
+      date: '06 Dec 2024',
+      amount: 5
+    },
+    {
+      type: 'OUTGO',
+      desc: 'Spent on Amazon buy Ps5',
+      date: '06 Dec 2024',
+      amount: 5
+    },
+    {
+      type: 'INCOME',
+      desc: 'Transferred from wallet',
+      date: '06 Dec 2024',
+      amount: 142
+    },
+    {
+      type: 'INCOME',
+      desc: 'Completed Task successfully',
+      date: '06 Dec 2024',
+      amount: 142
+    },
+    {
+      type: 'OUTGO',
+      desc: 'Spent on Amazon buy Ps5',
+      date: '06 Dec 2024',
+      amount: 20
+    }
   ]
 }
 
@@ -50,7 +84,7 @@ export const Wallet = () => {
           </div>
           {/* 2 cards */}
           <div className="flex justify-center w-full gap-6 flex-wrap">
-            {WALLET_PAGE_DATA.transactions.map(transaction => (
+            {WALLET_PAGE_DATA.transactionCards.map(transaction => (
               <StatCard
                 title={transaction.title}
                 value={transaction.value}
@@ -63,6 +97,22 @@ export const Wallet = () => {
         <div className="w-full px-4 py-3 bg-white hover:bg-white/80 cursor-pointer text-primary-700 text-center font-semibold text-lg rounded-xl">
           Withdraw Balance
         </div>
+      </div>
+      {/* Title + Description */}
+      <div className="w-full flex flex-col gap-1 font-sans">
+        <h1 className="font-bold text-2xl">{WALLET_PAGE_DATA.title}</h1>
+        <p className="text-xs text-gray-700">{WALLET_PAGE_DATA.desc}</p>
+      </div>
+      {/* Previous Transactions */}
+      <div className="flex flex-col gap-4 w-full overflow-auto">
+        {WALLET_PAGE_DATA.transactions.map(transaction => (
+          <TransactionCard
+            amount={transaction.amount}
+            date={transaction.date}
+            desc={transaction.desc}
+            type={transaction.type as ('OUTGO' | 'INCOME')}
+          />
+        ))}
       </div>
     </div>
   )
@@ -86,6 +136,27 @@ const StatCard = ({ title, value }: StatCardProps) => {
             {pointsToString(value)}
           </span> XP
         </p>
+      </div>
+    </div>
+  )
+}
+
+interface TransactionCardProps {
+  type: 'INCOME' | 'OUTGO',
+  desc: string,
+  date: string,
+  amount: number
+}
+
+export const TransactionCard = ({ amount, date, desc, type }: TransactionCardProps) => {
+  return (
+    <div className="bg-white rounded-lg w-full flex justify-between gap-4 px-4 py-2">
+      <div className="flex flex-col flex-1">
+        <h3 className="font-medium text-lg">{desc}</h3>
+        <p className="font-normal text-[10px]">{date}</p>
+      </div>
+      <div className={`font-bold text-sm ${type === 'INCOME' ? 'text-[#12B76A]' : 'text-[#F97066]'}`}>
+        <span className="font-semibold text-xl">{type === 'INCOME' ? '+' : '-'} {amount}</span> XP
       </div>
     </div>
   )
